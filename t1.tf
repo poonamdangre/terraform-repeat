2344ca9b71f8b66b5c4b590c56420a57f31415d1
poonamkosare <poonamkosare021@gmail.com>
2023-09-26 17:38:02 +0530

    commit7

--- t1.tf
+++ t1.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 resource "aws_instance" "my_instance" {
-    ami = "ami-053b0d53c279acc90 "
+    ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
     key_name = "main"
     tags = {
