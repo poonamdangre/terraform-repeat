667f1c0e67e11e20c7edf18c41661a5fab0e8aaa
poonamkosare <poonamkosare021@gmail.com>
2023-10-13 22:12:35 +0530

    commit1

--- t1.tf
+++ t1.tf
@@ -2,12 +2,12 @@ provider "aws" {
     region = "us-east-1"
 }
 resource "aws_instance" "my_instance" {
-    ami = "ami-053b0d53c279acc90"
+    ami = "ami-041feb57c611358bd"
     instance_type = "t2.micro"
-    key_name = "main"
+    key_name = "id_rsa"
     tags = {
         name = "myinstance"
         env = "devops"
     }
-    vpc_security_group_ids = ["sg-08e3b62ba55c6092d"]
+    vpc_security_group_ids = ["sg-03c4a3fb1b4f09ca8"]
 }
